ce51bcd1da92b8944dcf6b2380b6fe7fdbb90439
MickShannahan <beasticorn@gmail.com>
2020-10-20 16:13:59 -0600

    first commit, bout time i got this working

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,21 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'no-multiple-empty-lines': ['warn'],
+    'vue/no-unused-vars': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,107 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex flex-column align-items-center">
+        <img alt="Vue logo" src="../assets/logo.png" style="transform: rotate(-90deg);width: 25px;" />
+        <img
+          alt="Vue logo"
+          src="../assets/logo.png"
+          style="transform: rotate(-90deg);width: 25px;margin-top: -10px;"
+        />
+      </div>
+      <div>
+        <h3 style="color: green">loggr</h3>
+      </div>
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item" :class="{ active: $route.name == 'Home' }">
+          <router-link :to="{ name: 'Home' }" class="nav-link">Home</router-link>
+        </li>
+        <li
+          class="nav-item"
+          v-if="user.isAuthenticated"
+          :class="{ active: $route.name == 'Profile' }"
+        ></li>
+      </ul>
+      <span class="navbar-text">
+        <button class="btn btn-success" @click="login" v-if="!user.isAuthenticated">Login</button>
+
+        <div class="dropdown" v-else>
+          <div class="btn btn-secondary dropdown-toggle" @click="state.dropOpen = !state.dropOpen">
+            <img :src="user.picture" alt="user photo" height="40" class="rounded" />
+            <span class="mx-3">{{user.name}}</span>
+          </div>
+          <div
+            class="dropdown-menu p-0 list-group w-100"
+            :class="{show: state.dropOpen}"
+            @click="state.dropOpen = false"
+          >
+            <router-link :to="{ name: 'Profile' }">
+              <div class="list-group-item list-group-item-action hoverable">Profile</div>
+            </router-link>
+            <div class="list-group-item list-group-item-action hoverable" @click="logout">logout</div>
+          </div>
+        </div>
+      </span>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { profileService } from '../services/ProfileService'
+import { api } from '../services/AxiosService'
+import { AuthService } from '../services/AuthService'
+import { AppState } from '../AppState'
+import { computed, reactive } from 'vue'
+
+export default {
+  name: 'Navbar',
+  setup() {
+    const state = reactive({
+      dropOpen: false
+    })
+
+    return {
+      state,
+      user: computed(() => AppState.user),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        await AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-menu {
+  user-select: none;
+  display: block;
+  transform: scale(0);
+  transition: all 0.15s linear;
+}
+.dropdown-menu.show {
+  transform: scale(1);
+}
+
+.hoverable {
+  cursor: pointer;
+}
+a:hover {
+  text-decoration: none;
+}
+</style>
